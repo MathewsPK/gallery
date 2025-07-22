3f73793e514b6451e2dabd12f9b9149bc14c824e
MathewsPK <143189387+MathewsPK@users.noreply.github.com>
2025-07-22 18:04:32 +0530

    Update settings.py

--- park/settings.py
+++ park/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE  # Add Whitenoise
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'# Add Whitenoise
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
