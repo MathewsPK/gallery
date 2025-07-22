0b8a28fb4905ff4a5e986381def0c1b071649510
MathewsPK <143189387+MathewsPK@users.noreply.github.com>
2025-07-22 18:38:19 +0530

    Update views.py

--- homeapp/views.py
+++ homeapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.management import call_command
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required 
 from .models import Bird
 
 def home(request):
@@ -13,3 +15,8 @@ def birds_view(request):
 def bird_detail_view(request, pk):
     bird = get_object_or_404(Bird, pk=pk)
     return render(request, 'bird_detail.html', {'bird': bird})
+    @login_required  # Restrict to admin users
+
+def load_fixture(request):
+    call_command('loaddata', 'birds_fixture.json')
+    return HttpResponse("Data loaded successfully.")
